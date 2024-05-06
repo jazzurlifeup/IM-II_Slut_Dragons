d8650f867d7c119a81fcb4e28ccd2a5f36214246
Iliketheflowers <stefanie.hasler@stud.fhgr.ch>
2024-05-07 00:23:51 +0200

    cleaned up js
    linked the js and cleaned up everything in js and css only

--- JavaScript/character.js
+++ JavaScript/character.js
@@ -6,11 +6,11 @@ fetch('https://rickandmortyapi.com/api/character')
   .catch(error => console.error('Error fetching characters:', error));
 
 function displayCharacters(characters) {
-    const container = document.getElementById('characters');
-    characters.forEach(character => {
-        const characterElement = document.createElement('div');
-        characterElement.className = 'character-card'; // Adding a class for styling purposes
-        characterElement.innerHTML = `
+  const container = document.getElementById('characters');
+  characters.forEach(character => {
+    const characterElement = document.createElement('div');
+    characterElement.className = 'character-card'; // Adding a class for styling purposes
+    characterElement.innerHTML = `
             <h3>${character.name}</h3>
             <img src="${character.image}" alt="${character.name}">
             <p>Status: ${character.status}</p>
@@ -20,6 +20,6 @@ function displayCharacters(characters) {
             <p>Location: ${character.location.name}</p>
             <a href="${character.url}" target="_blank">More Info</a>
         `;
-        container.appendChild(characterElement);
-    });
+    container.appendChild(characterElement);
+  });
 }
